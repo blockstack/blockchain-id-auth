ca077ae2ecb0a229ff32e794e79acb0d01a4c980
spankratov <ss-pankratov@yandex.ru>
2017-05-24 14:12:06 +0300

    added fetch_app_manifest method for AuthRequest

--- blockchainauth/auth_request.py
+++ blockchainauth/auth_request.py
@@ -7,6 +7,7 @@
     :license: MIT, see LICENSE for more details.
 """
 
+from jwt import DecodeError
 import requests
 import requests.exceptions
 import time
@@ -33,7 +34,7 @@ class AuthRequest(AuthMessage):
     ]
 
     def __init__(self, private_key, domain_name, manifest_uri=None, redirect_uri=None,
-                 scopes=None, expires_after=None, crypto_backend=default_backend()):
+                 scopes=None, expires_after=None, crypto_backend=default_backend(), **kwargs):
         """ private_key should be provided in HEX, WIF or binary format 
             domain_name should be a valid domain
             manifest_uri should be a valid URI
@@ -81,11 +82,16 @@ class AuthRequest(AuthMessage):
             payload['iss'] = make_did_from_address(address)
         return payload
 
-    def fetch_app_manifest(self):
-        manifest_uri = self._payload()['manifest_uri']
-        
+    @classmethod
+    def fetch_app_manifest(cls, token):
+        # decode the token
         try:
-            return requests.get(manifest_uri).json()
+            decoded_token = cls.decode(token)
+        except DecodeError:
+            return None
+
+        try:
+            return requests.get(decoded_token['payload']['manifest_uri']).json()
         except (requests.exceptions.RequestException, ValueError):
             # ValueError for non-json responses
             return None
